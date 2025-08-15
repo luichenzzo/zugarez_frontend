cb19d26b625b5f2b0710de99215c04f182deecae
Lamsu1505 <131729688+Lamsu1505@users.noreply.github.com>
2025-08-14 22:38:28 -0500

    NavBar creada

---
+++ zugarez_frontend/src/Components/TopBar/TopBar.jsx
@@ -0,0 +1,24 @@
+
+import './TopBar.css';
+
+function TopBar() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">ZUGARES</a>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" href="#">Inicio</a>
+        <a class="nav-link" href="#">Quienes somos</a>
+        <a class="nav-link" href="#">Productos</a>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+  )
+  
+}
+
+export default TopBar;
--- zugarez_frontend/src/main.jsx
+++ zugarez_frontend/src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.jsx';
+import TopBar from './Components/TopBar/TopBar';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <TopBar/>
   </StrictMode>
 );
